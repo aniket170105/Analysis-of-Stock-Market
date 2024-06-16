b5e530faa0fe8e2580e8320f92ee4c3008bf78ee
Aniket Singh <b22cs009@iitj.ac.in>
2024-06-16 19:50:54 +0530

    Updated

--- getFundamental.py
+++ getFundamental.py
@@ -3,12 +3,15 @@
 
 import joblib
 import numpy as np
+from sklearn.pipeline import make_pipeline
 
 def appFundamental(list_of_ratios, sector):
-    loaded_pipeline = joblib.load(f'Fundamental Analysis/{sector}FundamentalModel.pkl')
+    scaler = joblib.load('Fundamental Analysis/scaler.pkl')
+    model = joblib.load('Fundamental Analysis/model.pkl')
+    pipeline = make_pipeline(scaler, model)
     temp = [list_of_ratios] ###Because model expect a 2D array
-    predictions = loaded_pipeline.predict(temp)
-    return predictions
+    predictions = pipeline.predict(temp)
+    return predictions[0]
 
 print(appFundamental([1.4600,0.988012,0.843312,4.673462,0.823741,0.456823,0.305076,0.316398,27.944112,0.369388,0.656100],'technologySector'))
 
